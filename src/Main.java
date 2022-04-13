d0d6fb6c3b3402bddc38ed96cef7ef6292ba446f
KseniyaGunbina <senyagunbina94@gmail.com>
2022-04-13 22:29:05 +0300

    Устранение замечаний по домашнему заданию

--- src/Main.java
+++ src/Main.java
@@ -2,40 +2,60 @@ import java.io.*;
 import java.nio.file.*;
 
 public class Main {
+
+    public static Path path = Path.of("c:", "Games");
+
+    public static Path appDirectory(String dirName) {
+        try {
+            return Files.createDirectories(path.resolve(dirName));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static Path appFile(String dir, String fileName) {
+        try {
+            if (fileName.equals(null)) {
+                return Files.createFile(path.resolve(dir));
+
+            } else {
+                return Files.createFile(path.resolve(dir).resolve(fileName));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
+
         StringBuilder stringBuilder = new StringBuilder();
         String log = null;
 
         try {
-            stringBuilder.append(Files.createDirectories(Path.of("c:\\Games\\src\\main")) + " add.");
-            stringBuilder.append(Files.createFile(Path.of("c:\\Games\\src\\main\\main.java")) + " add. \n");
-            stringBuilder.append(Files.createFile(Path.of("c:\\Games\\src\\main\\Utils.java")) + " add. \n");
+            stringBuilder.append(appDirectory("src/main") + " add.\n" +
+                    appFile("src/main","main.java") + " add.\n" +
+                    appFile("src/main","Utils.java") + " add.\n" +
+                    appDirectory("src/test") + " add.\n" +
+                    appDirectory("res/drawables") + " add.\n" +
+                    appDirectory("res/vectors") + " add.\n" +
+                    appDirectory("res/icons") + " add.\n" +
+                    appDirectory("savegames") + " add.\n" +
+                    appDirectory("temp") + " add.\n" +
+                    appFile("temp","temp.txt") + " add.\n");
 
-            stringBuilder.append(Files.createDirectories(Path.of("c:\\Games\\src\\test")) + " add. \n");
-
-            stringBuilder.append(Files.createDirectories(Path.of("c:\\Games\\res\\drawables")) + " add. \n");
-            stringBuilder.append(Files.createDirectories(Path.of("c:\\Games\\res\\vectors")) + " add. \n");
-            stringBuilder.append(Files.createDirectories(Path.of("c:\\Games\\res\\icons")) + " add. \n");
-
-            stringBuilder.append(Files.createDirectories(Path.of("c:\\Games\\res\\drawables")) + " add. \n");
-            stringBuilder.append(Files.createDirectories(Path.of("c:\\Games\\res\\vectors")) + " add. \n");
-            stringBuilder.append(Files.createDirectories(Path.of("c:\\Games\\res\\icons")) + " add. \n");
-
-            stringBuilder.append(Files.createDirectories(Path.of("c:\\Games\\savegames")) + " add. \n");
-
-            stringBuilder.append(Files.createDirectories(Path.of("c:\\Games\\temp")) + " add. \n");
-            stringBuilder.append(Files.createFile(Path.of("c:\\Games\\temp\\temp.txt")) + " add. \n");
-
-             log = stringBuilder.toString();
-//            System.out.println(log);
-        } catch (IOException e) {
+            log = stringBuilder.toString();
+            System.out.println(log);
+        } catch (NullPointerException e) {
             e.printStackTrace();
         }
 
-        try (FileWriter fileWriter = new FileWriter("c:\\Games\\temp\\temp.txt")) {
+        try (FileWriter fileWriter = new FileWriter("c:/Games/temp/temp.txt")) {
             fileWriter.write(log);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
+
